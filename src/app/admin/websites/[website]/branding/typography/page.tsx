061ec414b0cfb801729ce55f21bbb8e581355dda
Vijedra Choudhary <vijendrachoudhary3344@gmail.com>
2026-02-18 18:52:20 +0530

    design change code

--- src/app/admin/websites/[website]/branding/typography/page.tsx
+++ src/app/admin/websites/[website]/branding/typography/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import StaticTypography from "./StaticTypography";
 import TypographyPage from "@/app/admin/branding/typography/page";
-import PageMan from "./pageMAN";
+// import PageMan from "./pageMAN";
 
 const page = () => {
   return (
