2fac45f771aee752469fb6a69b19e73280549cc0
MANISH KUMAR GUPTA <manish.gupta.mech@gmail.com>
2026-02-25 15:33:37 +0530

    update banner

---
+++ src/app/admin/marketing/banners/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import BannerPage from '@/app/admin/websites/[website]/marketing/banners/page'
+const page = () => {
+    return (
+        <BannerPage />
+    )
+}
+
+export default page
